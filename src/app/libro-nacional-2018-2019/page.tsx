7af7caade9854c02cea196779019808307160426
fggonzalezz <fggonzalez@correo.um.edu.uy>
2025-11-10 21:48:39 -0300

    cositas x5

--- src/app/libro-nacional-2018-2019/page.tsx
+++ src/app/libro-nacional-2018-2019/page.tsx
@@ -252,33 +252,6 @@ export default function LibroNacionalPage() {
                   </Link>
                 </CardContent>
               </Card>
-
-              {/* Seminarios */}
-              <Card className="border-t-4 border-orange-500 hover:shadow-lg transition-shadow">
-                <CardContent className="p-6">
-                  <div className="flex items-center mb-4">
-                    <div className="bg-orange-100 rounded-full p-3 mr-4">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-orange-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                      </svg>
-                    </div>
-                    <h3 className="text-xl font-bold text-orange-700">Seminarios</h3>
-                  </div>
-                  <p className="text-gray-600 mb-4">
-                    Accede a grabaciones de seminarios impartidos por exolímpicos y entrenadores 
-                    para profundizar en temas específicos.
-                  </p>
-                  <Link
-                    href="/seminarios-exolimpicos"
-                    className="inline-flex items-center text-orange-600 hover:text-orange-800 font-medium"
-                  >
-                    Ver Seminarios
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
-                    </svg>
-                  </Link>
-                </CardContent>
-              </Card>
             </div>
 
             {/* Info Note */}
